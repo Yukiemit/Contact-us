cb43f25be36a8e395311c184ec78a00a2d9b1ab6
yukiemit <202310242@fit.edu.ph>
2025-03-18 12:46:07 +0800

    Links

--- script.js
+++ script.js
@@ -28,3 +28,13 @@ function closePopup() {
     let popup = document.getElementById("popup");
     popup.classList.remove("show");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const cartCount = document.getElementById("cart-count");
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cartCount.innerText = cart.length;
+
+});
